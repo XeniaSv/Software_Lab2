e911d077d7d5726c59f98ec3afdadcd1e044e7eb
Xenia <savelicheva123@gmail.com>
2020-03-01 19:58:53 +0300

    Test

--- BinaryParser.cs
+++ BinaryParser.cs
@@ -8,7 +8,7 @@ namespace Lab1{
         private Calculator calculator = new Calculator();
 
         public byte BinaryParserCalcMatch(byte[] sequence, char oper){
-            Match match = sequence.Parse(Endian.Big, (byte)0x01, (byte)0x02);
+            Match match = sequence.Parse(Endian.Big, (byte)0x02, (byte)0x03);
             byte[] matchedSequence = match.MatchedBytes;
             if (oper == '+'){
                 return calculator.Addition(matchedSequence[0], matchedSequence[1]);
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Lab1
         static void Main(string[] args)
         {
             Menu menu = new Menu();
-            menu.Commands();
+            //menu.Commands();
             BinaryParser parser = new BinaryParser();
             byte[] sequence = new byte[] {0x00,0x01,0x02,0x03};
             Console.WriteLine(parser.BinaryParserCalcMatch(sequence, '+'));
